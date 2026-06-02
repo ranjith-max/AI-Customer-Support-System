0b83f215e835d39977ad814897b3f3236947b8df
ranjith-max <gs8427299@gmail.com>
2026-06-02 16:09:34 +0530

    Add startup marker

--- backend/src/main/java/com/ranjith/backend/BackendApplication.java
+++ backend/src/main/java/com/ranjith/backend/BackendApplication.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		System.out.println(" ===  Ranjith Deployment Version");
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
